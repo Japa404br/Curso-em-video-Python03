515f4e5dbe0df6e245889476d979a1ddb48f4e9b
JapaScript <adriandev.uc@gmail.com>
2026-07-29 23:49:36 -0300

    feat: adicionar exercicio 10 - conversor de moeda BRL/USD/EUR

---
+++ PYTHON EXERCÍCIOS/ex010.py
@@ -0,0 +1,7 @@
+#conversor de salto na carteira para dolar e euro
+
+carteira = float(input('Quantos dinheiro você tem na carteira? R$'))
+dolar = carteira / 5.13
+euro = carteira / 5.88
+
+print('Com R$ {:.2f} você pode comprar US${:.2f} ou {:.2f}€'.format(carteira, dolar, euro))
